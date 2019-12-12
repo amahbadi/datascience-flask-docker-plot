c6f2d8e7edab3a880d763c3cdf3d2a96aa65032c
ALIK <aamahbadi@yahoo.com>
2019-12-12 16:42:47 -0500

    initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
 #--------ALI AKBAR MAHBADI :aamahbadi@yahoo.com----------
-import csv, os
 from flask import Flask, render_template
+import csv, os
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from io import BytesIO
+import base64
+
 
 app = Flask(__name__,
             static_url_path=''
--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 #--------ALI AKBAR MAHBADI :aamahbadi@yahoo.com----------
-from app import app, render_template,os
+from app import *
+
+
 count = 0
 
 def readcsv(filename, count):
@@ -8,14 +10,45 @@ def readcsv(filename, count):
         count += 1
         if count == 15:
             break
-        
+
 FILE = 'static/titanic.csv'
 iter_file = iter(readcsv(FILE, count))
 
-list_titanic = []
-for i in iter_file:
-    list_titanic.append(i.strip().split(','))
+list_data = pd.read_csv(FILE)
+
+def get_plot_data(field_name, list_data):
+    field = list(np.array(list_data[field_name]))
+    mu , sigma = np.mean(field), np.std(field)
+    plt.figure()
+    n, bins, patches = plt.hist(field, 50, density=True, facecolor='#FF1744', alpha=0.75)
+    plt.xlabel(field_name)
+    plt.ylabel('Probability')
+    plt.title('Histogram of %s' %(field_name))
+    plt.text(48, .03, r'$\mu={},\ \sigma={}$'.format(round(mu),round(sigma)))
+    plt.grid(color='k', linestyle='--', linewidth=0.5)
+    figfile = BytesIO()
+    plt.savefig(figfile, format='png')
+    figfile.seek(0)
+    figdata_png = base64.b64encode(figfile.getvalue())
+    return figdata_png
+
 
 @app.route('/')
+def plot_data_age():
+    result = get_plot_data('Age', list_data)
+    return render_template('plot_age.html', result=result.decode('utf8'))
+
+@app.route('/fare')
+def plot_data_fare():
+    result = get_plot_data('Fare', list_data)
+    return render_template('plot_fare.html', result=result.decode('utf8'))
+
+list_titanic = []
+@app.route('/table')
 def home():
-    return render_template('index.html', for_i = list_titanic)
+    for i in iter_file:
+        list_titanic.append(i.strip().split(','))
+    return render_template('table.html', for_i = list_titanic)
+
+if __name__ == "__main__":
+    app.run(debug=True)
